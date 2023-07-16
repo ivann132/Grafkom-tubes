8bcbd512dddd17934673f3fe01f547d290f179d9
ivann132 <106300613+ivann132@users.noreply.github.com>
2023-07-16 22:32:26 +0700

    Update ClippingDemo.java

--- ClippingDemo.java
+++ ClippingDemo.java
@@ -5,8 +5,6 @@ import java.awt.event.KeyEvent;
 import java.awt.event.MouseAdapter;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
-import java.util.ArrayList;
-import java.util.List;
 
 import javax.swing.JOptionPane;
 
